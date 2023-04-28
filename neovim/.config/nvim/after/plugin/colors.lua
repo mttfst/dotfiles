aaf1a3a9b6264f957a57d13f773fe4a7880b0557
Matthias Faust <faust@tropos.de>
2023-04-28 13:29:44 +0200

    Tweak Neovim Color Scheme
    The combination of treesitter and rose-pine let nearly every text appear in italic font. So I reconfigured it with more options.
    
    Most important, I disabled italics for all but comments.
    I also tweaked the telescope colours a bit.
    I'm not entirely happy with the red status line yet.

--- neovim/.config/nvim/after/plugin/colors.lua
+++ neovim/.config/nvim/after/plugin/colors.lua
@@ -1,11 +1,63 @@
-function ColorMyPencils(color)
-	color = color or 'rose-pine-moon'
-	-- color = color or 'rose-pine'
-	vim.cmd.colorscheme(color)
-	
---	vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
---	vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-end
+require('rose-pine').setup({
+	--- @usage 'auto'|'main'|'moon'|'dawn'
+	variant = 'auto',
+	--- @usage 'main'|'moon'|'dawn'
+	dark_variant = 'moon',
+	bold_vert_split = false,
+	dim_nc_background = false,
+	disable_background = true,
+	disable_float_background = true,
+	disable_italics = true,
 
-ColorMyPencils()
+	--- @usage string hex value or named color from rosepinetheme.com/palette
+	groups = {
+		background = 'base',
+		background_nc = '_experimental_nc',
+		panel = 'surface',
+		panel_nc = 'base',
+		border = 'highlight_med',
+		comment = 'muted',
+		link = 'iris',
+		punctuation = 'subtle',
 
+		error = 'love',
+		hint = 'iris',
+		info = 'foam',
+		warn = 'gold',
+
+		headings = {
+			h1 = 'iris',
+			h2 = 'foam',
+			h3 = 'rose',
+			h4 = 'gold',
+			h5 = 'pine',
+			h6 = 'foam',
+		}
+		-- or set all headings at once
+		-- headings = 'subtle'
+	},
+
+	-- Change specific vim highlight groups
+	-- https://github.com/rose-pine/neovim/wiki/Recipes
+	highlight_groups = {
+		-- make comments italic
+		Comment = { italic = true }, 
+
+		ColorColumn = { bg = 'rose' },
+
+		-- Blend colours against the "base" background
+		CursorLine = { bg = 'foam', blend = 10 },
+		StatusLine = { fg = 'love', bg = 'love', blend = 20 },
+		StatusLineNC = { fg = "subtle", bg = "surface" },
+
+		TelescopeBorder = { fg = "iris", bg = "none" },
+		TelescopeNormal = { bg = "none" },
+		TelescopePromptNormal = { bg = "none" },
+		TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+		TelescopeSelection = { fg = "text", bg = "muted" },
+		TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+	}
+})
+
+-- Set colorscheme after options
+vim.cmd('colorscheme rose-pine')
