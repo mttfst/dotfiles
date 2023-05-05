7104c677d9710659868e12583c9b7d944afe0bd3
Matthias Faust <faust@tropos.de>
2023-05-05 13:31:58 +0200

    Add LSP-Zero to Neovim
    Instead of the rather simple VimCompletesMe plugin I used in the last years, I like to try an LSP this time.
    
    I try here LSP-Zero.
    https://github.com/VonHeikemen/lsp-zero.nvim
    
    I set up a few languages that I might use and activated tab completion. But I don't understand it entirely at the moment.
    
    nvim/after/plugin/lsp.lua

---
+++ neovim/.config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,29 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+lsp.ensure_installed({
+  -- Replace these with whatever servers you want to install
+  'fortls',
+  'ltex',
+  'pyre',
+  'lua_ls'
+})
+
+
+lsp.setup()
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    ['<Tab>'] = cmp_action.tab_complete(),
+    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+  }
+})
+
+
+
