5532099c2ffbf980679347a753ad34c8f633b69c
Matthias Faust <faust@tropos.de>
2024-02-28 16:23:38 +0100

    Update Rose-Pine settings

--- neovim/.config/nvim/after/plugin/colors.lua
+++ neovim/.config/nvim/after/plugin/colors.lua
@@ -1,62 +1,74 @@
 require('rose-pine').setup({
-	--- @usage 'auto'|'main'|'moon'|'dawn'
-	variant = 'auto',
-	--- @usage 'main'|'moon'|'dawn'
-	dark_variant = 'moon',
-	bold_vert_split = false,
-	dim_nc_background = false,
-	disable_background = true,
-	disable_float_background = true,
-	disable_italics = true,
+    --- @usage 'auto'|'main'|'moon'|'dawn'
+    variant = 'auto',
+    --- @usage 'main'|'moon'|'dawn'
+    dark_variant = 'moon',
+    bold_vert_split = false,
+    dim_inactive_windows = false,
 
-	--- @usage string hex value or named color from rosepinetheme.com/palette
-	groups = {
-		background = 'base',
-		background_nc = '_experimental_nc',
-		panel = 'surface',
-		panel_nc = 'base',
-		border = 'highlight_med',
-		comment = 'muted',
-		link = 'iris',
-		punctuation = 'subtle',
+    styles = {
+        bold = true,
+        italic = false,
+        transparency = true,
+    },
 
-		error = 'love',
-		hint = 'iris',
-		info = 'foam',
-		warn = 'gold',
+    --- @usage string hex value or named color from rosepinetheme.com/palette
+    groups = {
+        panel = 'surface',
+        panel_nc = 'base',
+        border = 'highlight_med',
+        link = 'iris',
 
-		headings = {
-			h1 = 'iris',
-			h2 = 'foam',
-			h3 = 'rose',
-			h4 = 'gold',
-			h5 = 'pine',
-			h6 = 'foam',
-		}
-		-- or set all headings at once
-		-- headings = 'subtle'
-	},
+        error = "love",
+        hint = "iris",
+        info = "foam",
+        note = "pine",
+        todo = "rose",
+        warn = "gold",
 
-	-- Change specific vim highlight groups
-	-- https://github.com/rose-pine/neovim/wiki/Recipes
-	highlight_groups = {
-		-- make comments italic
-		Comment = { italic = true }, 
+        git_add = "foam",
+        git_change = "rose",
+        git_delete = "love",
+        git_dirty = "rose",
+        git_ignore = "muted",
+        git_merge = "iris",
+        git_rename = "pine",
+        git_stage = "iris",
+        git_text = "rose",
+        git_untracked = "subtle",
 
-		ColorColumn = { bg = 'rose' },
+        headings = {
+            h1 = 'iris',
+            h2 = 'foam',
+            h3 = 'rose',
+            h4 = 'gold',
+            h5 = 'pine',
+            h6 = 'foam',
+        }
+        -- or set all headings at once
+        -- headings = 'subtle'
+    },
 
-		-- Blend colours against the "base" background
-		CursorLine = { bg = 'foam', blend = 10 },
-		StatusLine = { fg = 'love', bg = 'love', blend = 20 },
-		StatusLineNC = { fg = "subtle", bg = "surface" },
+    -- Change specific vim highlight groups
+    -- https://github.com/rose-pine/neovim/wiki/Recipes
+    highlight_groups = {
+        -- make comments italic
+        Comment = { italic = true },
 
-		TelescopeBorder = { fg = "iris", bg = "none" },
-		TelescopeNormal = { bg = "none" },
-		TelescopePromptNormal = { bg = "none" },
-		TelescopeResultsNormal = { fg = "subtle", bg = "none" },
-		TelescopeSelection = { fg = "text", bg = "muted" },
-		TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
-	}
+        ColorColumn = { bg = 'rose' },
+
+        -- Blend colours against the "base" background
+        CursorLine = { bg = 'foam', blend = 10 },
+        StatusLine = { fg = 'love', bg = 'love', blend = 20 },
+        StatusLineNC = { fg = "subtle", bg = "surface" },
+
+        TelescopeBorder = { fg = "iris", bg = "none" },
+        TelescopeNormal = { bg = "none" },
+        TelescopePromptNormal = { bg = "none" },
+        TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+        TelescopeSelection = { fg = "text", bg = "muted" },
+        TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+    }
 })
 
 -- Set colorscheme after options
